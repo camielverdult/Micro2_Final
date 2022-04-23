924bbddcbadf1c304f55aff57cd6d868f959a164
camielverdult <cpg@verdult.com>
2022-04-23 16:06:06 +0200

    Update JSON.h

--- JSON.h
+++ JSON.h
@@ -13,7 +13,13 @@
 #define CAPACITY 20
 #define GROWTH 3
 #define STRING_LENGTH 15
-#define ENTRY_BUFF_LENGTH 30
+#define ENTRY_BUFF_LENGTH ((STRING_LENGTH * 2) + 10)
+
+#define or ||
+#define and &&
+#define not !
+#define true 1
+#define false 0
 
 typedef struct {
 	char key[STRING_LENGTH];
@@ -27,16 +33,17 @@ typedef struct {
 } JSONDictionary;
 
 void JSON_Dictionary_Init(JSONDictionary* dictionary) {
-	// Allocate a json entry array on the heap
-	dictionary->entries = malloc(sizeof(JSONEntry) * CAPACITY);
+
+    // Allocate a json entry array on the heap
+	dictionary->entries = (JSONEntry*)malloc(sizeof(JSONEntry) * CAPACITY);
 	dictionary->capacity = CAPACITY;
 	dictionary->size = 0;
 }
 
 void JSON_Entry_Init(JSONEntry* dictionary) {
     // Allocate a json entry array on the heap
-    memset(dictionary->key, 0, STRING_LENGTH);
-    memset(dictionary->value, 0, STRING_LENGTH);
+    memset(dictionary->key, '\0', STRING_LENGTH);
+    memset(dictionary->value, '\0', STRING_LENGTH);
 }
 
 void JSON_Check(JSONDictionary* dictionary, uint8_t new_length) {
@@ -51,60 +58,102 @@ void JSON_Check(JSONDictionary* dictionary, uint8_t new_length) {
 	
 	uint16_t new_capacity = dictionary->capacity * GROWTH;
 
-	dictionary->entries = realloc(dictionary->entries, sizeof(JSONEntry) * new_capacity);
+	dictionary->entries = (JSONEntry*)realloc(dictionary->entries, sizeof(JSONEntry) * new_capacity);
+    dictionary->capacity = new_capacity;
+}
+
+uint8_t JSON_String_Valid(const char* string) {
+    // This function checks if the passed string has a null-terminator character
+    // before reaching the max string length allowed
+    uint8_t i = 0;
+
+    while (string[i] != '\0') {
+        i++;
+        if (i == STRING_LENGTH) {
+            // This string is too long or has a broken null-terminator
+            return false;
+        }
+    }
+
+    return true;
 }
 
 void JSON_Set_String(JSONDictionary* dictionary, const char* key, const char* value) {
-	
-	JSON_Check(dictionary, dictionary->size + 1);
+
+    if (not JSON_String_Valid(key) or not JSON_String_Valid(value)) {
+        // Passed key or value is broken and unsafe to use, abort
+        return;
+    }
 
 	// Check if key already exists
 	for (uint8_t i = 0; i < dictionary->size; i++) {
-		if (strcmp(dictionary->entries[i].key, key) == 0) {
+		if (strncmp(dictionary->entries[i].key, key, STRING_LENGTH) == 0) {
 
-            if (strcmp(dictionary->entries[i].value, value) == 0) {
+            if (strncmp(dictionary->entries[i].value, value, STRING_LENGTH) == 0) {
                 return;
             }
 
 			// Key exists, update value
-			strcpy((char*)&dictionary->entries[i].value, value);
+			strncpy((char*)&dictionary->entries[i].value, value, STRING_LENGTH);
 
 			return;
 		}
 	}
 
+    JSON_Check(dictionary, dictionary->size + 1);
+
     JSON_Entry_Init(&dictionary->entries[dictionary->size]);
 	
 	// Key does not exist here, add it
-    strcpy((char*)&dictionary->entries[dictionary->size].key, key);
+    strncpy((char*)&dictionary->entries[dictionary->size].key, key, STRING_LENGTH);
 
 	//dictionary->entries[dictionary->size].value = value;
-    strcpy((char*)&dictionary->entries[dictionary->size].value, value);
+    strncpy((char*)&dictionary->entries[dictionary->size].value, value, STRING_LENGTH);
 
 	dictionary->size += 1;
 }
 
 void JSON_Set_Float(JSONDictionary* dictionary, const char* key, float value) {
+
+    if (not JSON_String_Valid(key)) {
+        // Passed key or value is broken and unsafe to use, abort
+        return;
+    }
+
     char buffer[STRING_LENGTH];
+    memset(buffer, '\0', STRING_LENGTH);
+
+    // TODO: find a replacement for this for AVR/arduino
     snprintf(buffer, STRING_LENGTH, "%f", value);
 
 	JSON_Set_String(dictionary, key, buffer);
 }
 
 void JSON_Set_Integer(JSONDictionary* dictionary, const char* key, uint16_t value) {
+
+    if (not JSON_String_Valid(key)) {
+        // Passed key or value is broken and unsafe to use, abort
+        return;
+    }
+
 	char buffer[STRING_LENGTH];
+    memset(buffer, '\0', STRING_LENGTH);
 
     snprintf(buffer, STRING_LENGTH, "%d", value);
 	JSON_Set_String(dictionary, key, buffer);
 }
 
 void JSON_Serialize_Entry(JSONEntry* entry, char* buffer, uint8_t buffer_length) {
+    memset(buffer, '\0', buffer_length);
 	snprintf(buffer, buffer_length, "\"%s\": \"%s\"", entry->key, entry->value);
 }
 
 void JSON_Serialize_Dictionary(JSONDictionary* dictionary, char* buffer, uint16_t buffer_length) {
 
-    memset(buffer, 0, buffer_length);
+    // Fill the entire buffer with null terminator characters so that strlen is
+    // less obliged to not find a null terminator string, strncpy will write these
+    // over anyway while building the dictionary
+    memset(buffer, '\0', buffer_length);
 
 	uint32_t buffer_index = 0;
 		
