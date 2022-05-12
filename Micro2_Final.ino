52f7bf765b7b84a692032939f96609b3139d675c
Camiel Verdult <cpg@verdult.com>
2022-05-12 11:27:23 +0200

    Update Micro2_Final.ino

--- Micro2_Final.ino
+++ Micro2_Final.ino
@@ -17,8 +17,51 @@
 #include "INA260.h"
 #include "JSON.h"
 
-int main(void)
-{
+void loop_uart() {
+  USART_Initialize();
+   
+  USART_Transmit_String("Starting up...\n");
+
+  while (1) {
+    JSONDictionary json;
+      
+    JSON_Dictionary_Init(&json);
+    
+    uint8_t red = rand() % 255;
+    JSON_Set_Integer(&json, (char*)"Red", red);
+    
+    uint8_t green = rand() % 255;
+    JSON_Set_Integer(&json, (char*)"Green", green);
+    
+    uint8_t blue = rand() % 255;
+    JSON_Set_Integer(&json, (char*)"Blue", blue);
+    
+    WS2812_Set(red, green, blue);
+  
+    uint16_t voltage = (uint16_t)(INA260_Voltage());
+    uint16_t current = (uint16_t)(INA260_Current());
+    uint16_t power = (uint16_t)(INA260_Power());
+     
+    JSON_Set_Integer(&json, (char*)"voltage", voltage);
+    JSON_Set_Integer(&json, (char*)"current", current);
+    JSON_Set_Integer(&json, (char*)"power", power);
+     
+    char buffer[300];
+    
+    USART_Transmit_String_Debug("Serializing JSON...\n");
+    
+    JSON_Serialize_Dictionary(&json, buffer, 300);
+    
+    USART_Transmit_String(buffer);
+    USART_Transmit_Char('\n');
+        
+    JSON_Cleanup(&json);
+    
+    _delay_ms(100);
+  }
+}
+
+int main(void) {
 	// Initialize USART
 	//USART_Initialize();
 	 
@@ -30,50 +73,5 @@ int main(void)
 	// Initialize INA260 (This will also initialize I2C)
 	INA260_Initialize();
 		
-	Serial.begin(9600);
-		
-    while(1)
-    {	
-        // TODO:: Please write your application code
-		// TestLeds();
-		// TestINA260();
-		
-		JSONDictionary json;
-			
-		JSON_Dictionary_Init(&json);
-		
-		uint8_t red = rand() % 255;
-		JSON_Set_Integer(&json, (char*)"Red", red);
-		
-		uint8_t green = rand() % 255;
-		JSON_Set_Integer(&json, (char*)"Green", green);
-		
-		uint8_t blue = rand() % 255;
-		JSON_Set_Integer(&json, (char*)"Blue", blue);
-		
-		WS2812_Set(red, green, blue);
-
-		uint16_t voltage = (uint16_t)(INA260_Voltage());
-		uint16_t current = (uint16_t)(INA260_Current());
-		uint16_t power = (uint16_t)(INA260_Power());
-		 
-		JSON_Set_Integer(&json, (char*)"voltage", voltage);
-		JSON_Set_Integer(&json, (char*)"current", current);
-		JSON_Set_Integer(&json, (char*)"power", power);
-		 
-		char buffer[300];
-		
-		USART_Transmit_String_Debug("Serializing JSON...\n");
-		
-		JSON_Serialize_Dictionary(&json, buffer, 300);
-		
-		//USART_Transmit_String(buffer);
-		//USART_Transmit_Char('\n');
-		
-		Serial.println(buffer);
-		
-		JSON_Cleanup(&json);
-		
-		_delay_ms(100);
-    }
+	loop_uart();
 }
